ac282e45d33823b2ba959d4686606937e768b9f8
FatoomMFayad <fatoomfayyad@gmail.com>
2021-10-31 20:08:43 +0200

    add styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <title>My Blog</title>
-    <body>
+    <link type="stylesheet" href="/app.css">
+    <body class="body">
         <h1>Hello World!!</h1>
     </body>
 </html>
