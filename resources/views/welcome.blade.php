9b2fdf527ac5dc4447801899854831af7e4f78a5
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-11-01 09:08:05 +0200

    fix css link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <title>My Blog</title>
-    <link type="stylesheet" href="/app.css">
+    <link href="{{ asset('/app.css') }}" rel="stylesheet">
     <article class="body">
         <h1>My article here</h1>
         <p>lorem episom lorem episom lorem episom lorem episom</p>
