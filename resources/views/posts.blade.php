5b7e989b35cf63d4095b374b2fbf0c7208c6c199
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-11-01 10:04:37 +0200

    fix posts links, redirect to home if file not exists

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,15 +3,15 @@
     <title>My Blog</title>
     <link href="{{ asset('/app.css') }}" rel="stylesheet">
     <article class="body">
-        <h1><a href="post">My First Post</a></h1>
+        <h1><a href="posts/my_first_post">My First Post</a></h1>
         <p>lorem episom lorem episom lorem episom lorem episom</p>
     </article>
     <article class="body">
-        <h1><a href="post">My Second Post here</a></h1>
+        <h1><a href="posts/my_second_post">My Second Post</a></h1>
         <p>lorem episom lorem episom lorem episom lorem episom</p>
     </article>
     <article class="body">
-        <h1><a href="post">My Third Post here</a></h1>
+        <h1><a href="posts/my_third_post">My Third Post</a></h1>
         <p>lorem episom lorem episom lorem episom lorem episom</p>
     </article>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,11 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug){
-    $post = file_get_contents(__DIR__."/../resources/posts/{$slug}.html");
+    $path = __DIR__."/../resources/posts/{$slug}.html";
+    if(! file_exists($path)){
+        return redirect('/');
+    }
+    $post = file_get_contents($path);
     return view('post', [
         'post' => $post
     ]);
