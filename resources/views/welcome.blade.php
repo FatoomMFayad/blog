0674cfc3d2273ccd329c7696f08dfec3bf6de475
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-11-01 09:28:04 +0200

    adding multiple posts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,15 @@
     <title>My Blog</title>
     <link href="{{ asset('/app.css') }}" rel="stylesheet">
     <article class="body">
-        <h1>My article here</h1>
+        <h1><a href="post">My First Post</a></h1>
+        <p>lorem episom lorem episom lorem episom lorem episom</p>
+    </article>
+    <article class="body">
+        <h1><a href="post">My Second Post here</a></h1>
+        <p>lorem episom lorem episom lorem episom lorem episom</p>
+    </article>
+    <article class="body">
+        <h1><a href="post">My Third Post here</a></h1>
         <p>lorem episom lorem episom lorem episom lorem episom</p>
     </article>
 </html>
