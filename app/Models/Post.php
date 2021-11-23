5afa28fdb17df3c553babd97a4b22b9d80c561d0
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-11-23 09:55:31 +0200

    Create Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,22 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function find($slug)
-    {
-        if(! file_exists($path = resource_path("/posts/{$slug}.html"))){
-            throw new ModelNotFoundException();
-        }
-        return cache()->remember("posts.{$slug}", 5, fn() => file_get_contents($path));
-    }
-
-    public static function all()
-    {
-        $files = File::files(resource_path("/posts"));
-        return array_map(fn($file)=>$file->getContents(),$files);
-    }
+    use HasFactory;
 }
